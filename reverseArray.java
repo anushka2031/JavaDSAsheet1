48eab8a2f0f7c21b88de94a1b978ad9d5b602473
anushka2031 <anushkatiwari2031@gmail.com>
2025-05-11 20:25:08 +0530

    ques2

---
+++ reverseArray.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class reverseArray {
+
+    public static void main(String[] args) {
+        
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int n= in.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter elements of array:");
+
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        System.out.println("The reversed elements are");
+        int temp;
+        for (int i=0;i<arr.length;i++){
+            for (int j=arr.length-1;j>=0;j--){
+                //reversing the elements of array using temprary variable
+                if(i<j){
+                   temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+                   System.out.println(arr[i]);
+                }
+            }
+        }
+        
+    }
+    
+}
