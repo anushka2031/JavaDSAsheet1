f20831ddf5c109481de5d64798ffac8f9beaaf55
anushka2031 <anushkatiwari2031@gmail.com>
2025-05-19 10:01:49 +0530

    Ques2

--- reverseArray.java
+++ reverseArray.java
@@ -1,13 +1,35 @@
 
 //for user input
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class reverseArray {
+
+    static void Reverse(int[] arr){
+
+        int temp;
+        int l=0;
+        int h=arr.length-1;
+        for(int i=0;i<arr.length;i++){
+
+            if(l<h){
+                temp=arr[h];
+                arr[h]=arr[l];
+                arr[l]=temp;
+            }
+
+            l++;
+            h--;
+
+        }
+        for (int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
 
     public static void main(String[] args) {
 
         
-        int[] arr={1,2,3};
+        int[] arr={1,2,3,4,5};
         
         //user input
         // Scanner in= new Scanner(System.in);
@@ -20,21 +42,14 @@ public class reverseArray {
         //     arr[i]=in.nextInt();
         // }
 
-        System.out.println("The reversed elements are");
-        int temp;
-        for (int i=0;i<arr.length;i++){
-            for (int j=arr.length-1;j>=0;j--){
-                //reversing the elements of array using temprary variable
-                if(i<j){
-                   temp=arr[i];
-                   arr[i]=arr[j];
-                   arr[j]=temp;
-                   System.out.println(arr[i]);
-                
-                }
-            }
-          
+        System.out.println("original array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
         }
+
+        System.out.println("The reversed elements are");
+        
+        Reverse(arr);
 
     }
     
