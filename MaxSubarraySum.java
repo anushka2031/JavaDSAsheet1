88f581b471fb4ed3db5de91afb52eb6afbc72f84
anushka2031 <anushkatiwari2031@gmail.com>
2025-05-25 19:38:45 +0530

    Ques8

---
+++ MaxSubarraySum.java
@@ -0,0 +1,36 @@
+public class MaxSubarraySum {
+    
+    /*The maximum subarray problem involves the adjacent sequence of 
+     * numbers within an array that has the largest sum.
+     */
+    static void sbarray(int[] arr){
+       
+        int sum;
+        int max=arr[0];
+        int a=0;
+
+        //finds the max subarray sum for subarrays with three element.
+        for(int i=0;i<(arr.length-2); i++){
+           
+            sum=arr[i]+arr[i+1]+arr[i+2];
+            //calculates the sum of three continuos indexs.
+
+            if(sum>max){
+                max=sum;
+                a=i;
+            }   
+        }         
+        
+        System.out.println("the sum is: "+max);
+        System.out.println("the subarray consists of elements "+arr[a]+" "+arr[a+1]+" "+arr[a+2]);
+       
+    } 
+
+    public static void main(String[] args) {
+        
+        int[] arr={2,-7,0,5,6,-3,0,1};
+        sbarray(arr);
+    }
+
+    
+}
