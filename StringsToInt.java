6dd8dcc84b7509193823637f93924451cabca39f
anushka2031 <anushkatiwari2031@gmail.com>
2025-06-15 13:48:13 +0530

    Ques28

--- StringsToInt.java
+++ StringsToInt.java
@@ -5,7 +5,13 @@ public class StringsToInt {
         int res=0;
         for (int i=0 ; i<str.length() ; i++){
             char c = str.charAt(i);
-            res = res * 10 + c -'0';
+            if( c >= '0' && c <= '9'){
+                res = res * 10 + c -'0';
+            }
+            else{
+                break;
+            }
+            
         }
         System.out.println(res);
     }
