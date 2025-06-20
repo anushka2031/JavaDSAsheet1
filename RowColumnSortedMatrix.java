f417a2f68878dc4db571ec5763c502b46cfac5bd
anushka2031 <anushkatiwari2031@gmail.com>
2025-06-20 12:57:55 +0530

    Ques38

---
+++ RowColumnSortedMatrix.java
@@ -0,0 +1,37 @@
+public class RowColumnSortedMatrix {
+
+    public static int searchmatri(int[][] matrix , int target){
+        
+        int row = 0;
+        int col = matrix[0].length-1;
+        
+        while (row < matrix.length && col >= 0) {
+            if(target == matrix[row][col]){
+                return 1;
+            }
+            else if(target > matrix[row][col]){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        
+        int[][] matrix = { {3,30,38} , {20,52,54} , {35,60,69} };
+        int x = 35;
+
+        if(searchmatri(matrix, x) == 1){
+            System.out.println("The target element " + x + " is found in the given matrix.");
+        }
+        else{
+            System.out.println("The target element " + x + " is NOT found in the given matrix.");
+        }
+    }
+    
+}
