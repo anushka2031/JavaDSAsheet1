9406ec3dbe05e558353389f4ce7f0ff5a2063142
anushka2031 <anushkatiwari2031@gmail.com>
2025-07-11 21:32:43 +0530

    Ques48

--- MedianRowSortedMatrix.java
+++ MedianRowSortedMatrix.java
@@ -54,6 +54,14 @@ public class MedianRowSortedMatrix {
         int r=matrix.length;
         int c=matrix[0].length;
 
+        System.out.println("Matrix: ");
+        for(int i=0 ; i<r ; i++){
+            for(int j=0 ; j<c ; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
         System.out.println("The median is: " + median(matrix, r, c));
     }
 }
